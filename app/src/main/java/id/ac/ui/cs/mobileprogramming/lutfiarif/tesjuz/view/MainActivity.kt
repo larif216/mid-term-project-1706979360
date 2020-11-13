da2d935132c53995b6259de5594b07c631c2505e
larif216 <muhamadlutfiarif10@gmail.com>
2020-11-13 21:49:23 +0700

    Add main empty activity

--- app/src/main/java/id/ac/ui/cs/mobileprogramming/lutfiarif/tesjuz/view/MainActivity.kt
+++ app/src/main/java/id/ac/ui/cs/mobileprogramming/lutfiarif/tesjuz/view/MainActivity.kt
@@ -1,5 +1,6 @@
 package id.ac.ui.cs.mobileprogramming.lutfiarif.tesjuz.view
 
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import id.ac.ui.cs.mobileprogramming.lutfiarif.tesjuz.R
